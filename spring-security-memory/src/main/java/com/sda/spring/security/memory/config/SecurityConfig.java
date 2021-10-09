50defc3e63feb6b209936e5a3b359fadfef68ceb
Costi <costi_georgescu2000@yahho.com>
2021-10-09 15:58:24 +0300

    final commit spring

--- spring-security-memory/src/main/java/com/sda/spring/security/memory/config/SecurityConfig.java
+++ spring-security-memory/src/main/java/com/sda/spring/security/memory/config/SecurityConfig.java
@@ -3,10 +3,12 @@ package com.sda.spring.security.memory.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     //authentication
@@ -35,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    private PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
